cbf50c0932db372b3525053474d2c706a39f50b5
TechGenie-awake <techgenie2050@gmail.com>
2026-03-10 22:15:00 +0530

    add event controller

---
+++ backend/src/controllers/EventController.js
@@ -0,0 +1,69 @@
+const EventService = require('../services/EventService');
+
+class EventController {
+  constructor() {
+    this.eventService = new EventService();
+    this.getAllEvents = this.getAllEvents.bind(this);
+    this.getEventById = this.getEventById.bind(this);
+    this.createEvent = this.createEvent.bind(this);
+    this.updateEvent = this.updateEvent.bind(this);
+    this.deleteEvent = this.deleteEvent.bind(this);
+    this.checkAvailability = this.checkAvailability.bind(this);
+  }
+
+  async getAllEvents(req, res, next) {
+    try {
+      const events = await this.eventService.getAllEvents(req.query);
+      res.json({ success: true, events });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getEventById(req, res, next) {
+    try {
+      const event = await this.eventService.getEventById(req.params.id);
+      res.json({ success: true, event });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async createEvent(req, res, next) {
+    try {
+      const event = await this.eventService.createEvent(req.body);
+      res.status(201).json({ success: true, event });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async updateEvent(req, res, next) {
+    try {
+      const event = await this.eventService.updateEvent(req.params.id, req.body);
+      res.json({ success: true, event });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async deleteEvent(req, res, next) {
+    try {
+      await this.eventService.deleteEvent(req.params.id);
+      res.json({ success: true, message: 'Event deleted' });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async checkAvailability(req, res, next) {
+    try {
+      const data = await this.eventService.checkAvailability(req.params.id);
+      res.json({ success: true, ...data });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = EventController;
